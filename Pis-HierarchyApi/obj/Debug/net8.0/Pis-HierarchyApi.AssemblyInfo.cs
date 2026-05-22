e5e13c1a4f48b44796d97ea516a83e20fd156a97
bezawitsebsbe <167539304+bezawitsebsbe@users.noreply.github.com>
2026-05-22 14:02:12 +0300

    docker added

--- Pis-HierarchyApi/obj/Debug/net8.0/Pis-HierarchyApi.AssemblyInfo.cs
+++ Pis-HierarchyApi/obj/Debug/net8.0/Pis-HierarchyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pis-HierarchyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098445a70fcb5eeb4bf65dba231390dedadb5424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3625a8aa81fd8e479e40cef05aefc2d39a82377")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pis-HierarchyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pis-HierarchyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
